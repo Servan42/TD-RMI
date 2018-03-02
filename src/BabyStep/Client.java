0d69dedd8d1aa7c7a6b28f55081419a280a0a08e
ProfesseurSid <salutlesmanchots@hotmail.fr>
2018-03-02 10:21:26 +0100

    Implémentation du server

--- src/BabyStep/Client.java
+++ src/BabyStep/Client.java
@@ -1,5 +1,5 @@
 package BabyStep;
 
 public class Client {
-
+	
 }
--- src/BabyStep/Server.java
+++ src/BabyStep/Server.java
@@ -1,5 +1,16 @@
 package BabyStep;
 
+import java.rmi.registry.LocateRegistry;
+
 public class Server {
-	
+	public static void main(String args[]) {
+		try {
+			FrenchToEnglish obj = new FrenchToEnglishImpl();
+			
+			LocateRegistry.createRegistry(2001);
+			java.rmi.Naming.bind("localhost:2001/FrenchToEnglishTranslator", obj);
+		} catch(Exception e) {
+			System.out.println("C'est la panique");
+		}
+	}
 }
