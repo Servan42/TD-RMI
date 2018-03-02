9ba8540754682d3ab811b6b19c2df9eee29edd59
Servan42 <charlot.servan@gmail.com>
2018-03-02 11:29:57 +0100

    Creation Client

--- src/BabyStep/Client.java
+++ src/BabyStep/Client.java
@@ -1,5 +1,19 @@
 package BabyStep;
 
 public class Client {
-	
+	public static void main(String args[]){
+		// Crée et installe un Security Manager, necessaire seulement si le code est téléchargé
+//		if(System.getSecurityManager() == null){
+//			System.setSecurityManager(new SecurityManager());
+//		}
+		try{
+			// Si rmiregistery ne tourne pas sur la machine courante, il faut
+			// remplacer localhost par le nom de la machine qui héberge le rmiregisteri
+			FrenchToEnglish obj = (FrenchToEnglish) java.rmi.Naming.lookup("//localhoast:2001/FrenchToEnglishTranslator");
+			String word = obj.translate(args[0]);
+			System.out.println(word);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 }
