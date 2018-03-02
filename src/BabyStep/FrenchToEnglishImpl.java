b883067a4abd1bf51cca20f55cd02aedc9ec45a9
ProfesseurSid <salutlesmanchots@hotmail.fr>
2018-03-02 10:30:24 +0100

    Implémentation d'une traduction mot-à-mot pour un ensemble réduit

--- src/BabyStep/FrenchToEnglishImpl.java
+++ src/BabyStep/FrenchToEnglishImpl.java
@@ -11,7 +11,21 @@ public class FrenchToEnglishImpl extends UnicastRemoteObject implements FrenchTo
 	
 	@Override
 	public String translate(String word) throws RemoteException {
-		String result = "Monika";
+		String result;
+		switch(word) {
+		case "Monika": result = "Monique";
+		case "Monique": result = "Monika";
+		case "Tea": result = "Thé";
+		case "Thé": result = "Tea";
+		case "BDE": result = "FDP";
+		case "FDP": result = "BDR";
+		case "Nique": result = "le CROUS";
+		case "Test": result = "AAAAAAH ERREUR ERREUR ABORT MISSION";
+		case "Amina": result = "mina hé hé";
+		case "Français": result = "English";
+		case "English": result = "Français";
+		default: result = "BIEN TENTE DUDE";
+		}
 		return result;
 	}
 
